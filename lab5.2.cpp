660850ea561a20b623b8ceb0a8744e7b28f669b2
vasyl1sidenko <gus123810@gmail.com>
2025-11-05 00:02:30 +0200

    Update lab5.2.cpp

--- lab5.2.cpp
+++ lab5.2.cpp
@@ -1,4 +1,4 @@
-// Lab_05.2.cpp
+// Lab_05.2cpp
 // Сіденко Василь
 // Лабораторна робота №5.2
 // Обчислення суми ряду Тейлора за допомогою функцій
@@ -9,12 +9,12 @@
 #include <cmath>
 using namespace std;
 
-void S(const double x, const double eps, int &n, double &s);
-void A(const double x, const int n, double &a);
+double S(const double x, const double eps, int &n);
+double A(const double x, const int n, double a);
 
 int main()
 {
-    double xp, xk, x, dx, eps, s = 0;
+    double xp, xk, x, dx, eps, s;
     int n = 0;
 
     cout << "xp = "; cin >> xp;
@@ -23,47 +23,49 @@ int main()
     cout << "eps = "; cin >> eps;
 
     cout << fixed;
-    cout << "-----------------------------------------------------------" << endl;
+    cout << "--------------------------------------------------------" << endl;
     cout << "|" << setw(7) << "x" << " |"
          << setw(12) << "ln(1+x)" << " |"
-         << setw(12) << "S(x)" << " |"
-         << setw(7) << "n" << " |"
-         << endl;
-    cout << "-----------------------------------------------------------" << endl;
+         << setw(12) << "S" << " |"
+         << setw(7) << "n" << " |" << endl;
+    cout << "--------------------------------------------------------" << endl;
 
     x = xp;
     while (x <= xk)
     {
-        S(x, eps, n, s);
+        s = S(x, eps, n);
         cout << "|" << setw(7) << setprecision(2) << x << " |"
              << setw(12) << setprecision(6) << log(1 + x) << " |"
              << setw(12) << setprecision(6) << s << " |"
-             << setw(7) << n << " |"
-             << endl;
+             << setw(7) << n << " |" << endl;
         x += dx;
     }
 
-    cout << "-----------------------------------------------------------" << endl;
+    cout << "--------------------------------------------------------" << endl;
     return 0;
 }
 
-void S(const double x, const double eps, int &n, double &s)
+double S(const double x, const double eps, int &n)
 {
     n = 0;
-    double a = x;
-    s = a;
+    double a = x; // перший член ряду при n=0
+    double s = a;
 
     do {
         n++;
-        A(x, n, a);
+        a = A(x, n, a);
         s += a;
-    } while (abs(a) >= eps);
+    } while (fabs(a) >= eps);
+
+    return s;
 }
 
-void A(const double x, const int n, double &a)
+double A(const double x, const int n, double a)
 {
-    // рекурентне співвідношення:
-    // a_n = (-1)^n * x^(n+1) / (n+1)
+    // Рекурентне співвідношення:
+    // a_n = (-1)^n * x^(n+1)/(n+1)
     // => a_n = a_(n-1) * (-1) * x * n / (n+1)
-    a *= (-1) * x * n / (n + 1);
+    double R = (-1) * x * n / (n + 1);
+    a *= R;
+    return a;
 }
